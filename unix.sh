5e1594af6c5d0d6d2e429a3b18b9851511528968
alonayurchenko <95172471+alonayurchenko@users.noreply.github.com>
2021-12-11 18:33:50 +0100

    Update unix.sh

--- unix.sh
+++ unix.sh
@@ -1,1 +1,23 @@
+#prepare data directories
 
+cd ./projects
+mkdir project
+#download data
+
+cp /data-shared/vcf_examples/luscinia_vars.vcf.gz . 
+
+#unzip
+
+gunzip luscinia_vars.vcf.gz   
+
+#choose only chromosomes and create «chr» with this data
+
+cut -f1 luscinia_vars.vcf |grep -v '^#' > chr 
+
+#choose only quality and create «qual» with this data
+
+cut -f6 luscinia_vars.vcf |grep -v '^#' > qual
+
+#create «test» with two columns (chromosomes and quality)
+
+paste chr qual > test
